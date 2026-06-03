c42ba0c405520dc19e60b703eb137d4df122f557
lustresense <farchandm@gmail.com>
2026-06-03 11:43:11 +0700

    chore: remove frontend source handoff

--- src/app/components/admin/index.ts
+++
@@ -1,6 +0,0 @@
-export { AdminEventsDatabase } from './AdminEventsDatabase';
-export { AdminOverview } from './AdminOverview';
-export { AdminReportsDatabase } from './AdminReportsDatabase';
-export { AdminUsersDatabase } from './AdminUsersDatabase';
-export { safePoints } from './adminDashboardUtils';
-export { useAdminDashboardData } from './useAdminDashboardData';
