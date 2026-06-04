9a586235eb0b72e389a00963f7459b4ba45b210d
SamlekOM User <user@samlekom.dev>
2026-06-04 18:53:19 +0700

    Remove src folder

--- src/app/components/landing/types.ts
+++
@@ -1,8 +0,0 @@
-export type LandingNavigatePage = "login" | "register" | "collaboration" | "about" | "faq";
-
-export interface LeaderboardEntry {
-  rank: number;
-  kelurahan: string;
-  kecamatan: string;
-  xp: number;
-}
